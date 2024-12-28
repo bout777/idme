6828d50e0b3d0bc04b388ad30e80e36ef79e0bbd
bout777 <2917898087@qq.com>
2024-12-28 13:50:21 +0800

    debug完成

--- src/main/java/com/idme/server/config/WebMvcConfiguration.java
+++ src/main/java/com/idme/server/config/WebMvcConfiguration.java
@@ -1,73 +1,73 @@
-//package com.idme.server.config;
-//
-//import com.idme.server.interceptor.JwtTokenUserInterceptor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.converter.HttpMessageConverter;
-//import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-//
-//
-//
-//import java.util.List;
-//
-///**
-// * 配置类，注册web层相关组件
-// */
-//@Configuration
-//@Slf4j
-//public class WebMvcConfiguration extends WebMvcConfigurationSupport {
-//
-//    @Autowired
-//    private JwtTokenUserInterceptor jwtTokenUserInterceptor;
-//
-//
-//    /**
-//     * 注册自定义拦截器
-//     *
-//     * @param registry
-//     */
-//    protected void addInterceptors(InterceptorRegistry registry) {
-//
-//        registry.addInterceptor(jwtTokenUserInterceptor)
-//                .addPathPatterns("/**")
-//                .excludePathPatterns("/user/login")
-//                .excludePathPatterns("/user/register")
-//                ;
-//
-//
-//    }
-//
-//    protected void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("*") // 允许的前端源
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowedHeaders("*")
-//                .allowCredentials(false);
-//    }
-//
-//    /**
-//     * 设置静态资源映射
-//     *
-//     * @param registry
-//     */
-//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
-//        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-//    }
-//
-//
-//    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-//
-//        //创建消息转换器对象
-//        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
-//        //设置对象转换器，底层使用Jackson将Java对象转为json
-//        messageConverter.setObjectMapper(new com.idme.common.json.JacksonObjectMapper());
-//        //将上面的消息转换器对象追加到mvc框架的转换器集合中
-//        converters.add(0, messageConverter);
-//    }
-//}
+package com.idme.server.config;
+
+import com.idme.server.interceptor.JwtTokenUserInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+
+
+import java.util.List;
+
+/**
+ * 配置类，注册web层相关组件
+ */
+@Configuration
+@Slf4j
+public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+
+    @Autowired
+    private JwtTokenUserInterceptor jwtTokenUserInterceptor;
+
+
+    /**
+     * 注册自定义拦截器
+     *
+     * @param registry
+     */
+    protected void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(jwtTokenUserInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/user/login")
+                .excludePathPatterns("/user/register")
+                ;
+
+
+    }
+
+    protected void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // 允许的前端源
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
+
+    /**
+     * 设置静态资源映射
+     *
+     * @param registry
+     */
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+        //创建消息转换器对象
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        //设置对象转换器，底层使用Jackson将Java对象转为json
+        messageConverter.setObjectMapper(new com.idme.common.json.JacksonObjectMapper());
+        //将上面的消息转换器对象追加到mvc框架的转换器集合中
+        converters.add(0, messageConverter);
+    }
+}
